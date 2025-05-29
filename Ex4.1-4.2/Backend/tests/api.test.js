3096b238cd2a6c1d9380fb8fcf52c3b1bee4b795
Shubhanker01 <shubhanker40@gmail.com>
2025-05-29 21:38:37 +0530

    beforeEach in testing

--- Ex4.1-4.2/Backend/tests/api.test.js
+++ Ex4.1-4.2/Backend/tests/api.test.js
@@ -1,9 +1,32 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
+const blogSchema = require('../models/blog.models')
 const supertest = require('supertest')
 const app = require('../index')
 const api = supertest(app)
 
+const initialBlogs = [
+    {
+        title: "my title",
+        author: "author1",
+        url: "http://author1blogs",
+        likes: 299,
+    },
+    {
+        title: "my title2",
+        author: "author2",
+        url: "http://author1blogs",
+        likes: 399,
+    },
+]
+
+beforeEach(async () => {
+    await blogSchema.deleteMany({})
+    let blogObject = new blogSchema(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new blogSchema(initialBlogs[1])
+    await blogObject.save()
+})
 test('notes are returned as json', async () => {
     await api.get('/blogs/api/getall')
         .expect(200)
